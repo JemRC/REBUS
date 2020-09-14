68e86c2869661e75f4d05bdfdc951be002e32bf7
JemRC <69658441+JemRC@users.noreply.github.com>
2020-09-14 13:04:03 +1200

    Mapping (Removed Tiled2Unity)
    Removed Tiled2Unity, because Unity has its own Tilemaps system already. Might as well use it

--- Rebus/Assets/Maps/bofhblk.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="bofhblk" tilewidth="16" tileheight="16" tilecount="1000" columns="20">
- <image source="../Art/bofhblk.png" width="320" height="800"/>
-</tileset>
