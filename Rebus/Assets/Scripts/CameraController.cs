9e3d3f65a60e2ab125e31bf8c763907712dcaaa3
JemRC <69658441+JemRC@users.noreply.github.com>
2020-09-14 15:18:49 +1200

    Testing Movement on Map

--- Rebus/Assets/Scripts/CameraController.cs
+++ Rebus/Assets/Scripts/CameraController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    GameObject player;
+    bool followPlayer = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (followPlayer == true)
+        {
+            camFollowPlayer();
+        }
+    }
+
+    public void setFollowPlayer(bool val)
+    {
+        followPlayer = val;
+    }
+
+    void camFollowPlayer()
+    {
+        Vector3 newPos = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        this.transform.position = newPos;
     }
 }
+
